e2f0df49ce24033aff7fcf242c87516c1f707f88
cootook <cootook@gmail.com>
2021-01-30 21:14:26 +0300

    fix: delete dublicate styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './App.css';
-
-
-
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
