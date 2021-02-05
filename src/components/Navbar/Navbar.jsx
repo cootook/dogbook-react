4b5747adb0d385b4f302da5c647054bae30cb0c1
cootook <cootook@gmail.com>
2021-02-05 23:12:29 +0300

    feat: add react router for links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 import stl from './Navbar.module.scss';
-import Link from './Link/Link';
+import NavLink from './NavLink/NavLink';
 // import profileLogo from '../images/pets-charly.png';
 
 export default function Navbar() {
   return (
     <nav className={stl.container}>
-      <div className={`${stl.profile_logo__container}`}>
-        <a href="#1" className={`${stl.item__link} ${stl.profile_logo}`}>
-        </a></div>
-      <Link linkTitle='News' />
-      <Link linkTitle='Profile' />
-      <Link linkTitle='Messages' />
-      <Link linkTitle='Communities' />
-      <Link linkTitle='Spots' />
-      <Link linkTitle='Clinics' />
-      <Link linkTitle='Instructors' />
-      <Link linkTitle='Map' />
-      <Link linkTitle='Settings' />
+      <BrowserRouter>
+        <div className={`${stl.profile_logo__container}`}>
+          <Link to='Profile' className={`${stl.item__link} ${stl.profile_logo}`}>
+          </Link>
+        </div>
+
+        <NavLink linkTitle='News' />
+        <NavLink linkTitle='Profile' />
+        <NavLink linkTitle='Messages' />
+        <NavLink linkTitle='Communities' />
+        <NavLink linkTitle='Spots' />
+        <NavLink linkTitle='Clinics' />
+        <NavLink linkTitle='Instructors' />
+        <NavLink linkTitle='Map' />
+        <NavLink linkTitle='Settings' />
+      </BrowserRouter>
     </nav>
   )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import s from './Profile.scss';
 
 export default function Profile() {
   return (
